8986eb27875169828567995939bea0ebbb2f4b2c
cx <chixuchixu@hotmail.com>
2017-08-24 11:08:02 +0800

    parse config items size <path size="1">xxx</path>

--- Scripts/Utils/ConfigLoader.cs
+++ Scripts/Utils/ConfigLoader.cs
@@ -50,15 +50,15 @@ public class ConfigLoader
 		yield return Request.ReadRemote (url, str => remoteConfig = XDocument.Parse(str).Root);
 		string platform = GetPlatformName ();
 
-		List<string> filenames = new List<string> ();
+		Dictionary<string, float> filenames = new Dictionary<string, float> ();
 		if (remoteConfig == null) {
 			Debug.Log ("remoteConfig = null");
 		} else if (localConfig == null ) {
 			Debug.Log ("remoteConfig != null");
-			fileSize = Xml.Float(remoteConfig.Element ("all"), "size");
+			//fileSize = Xml.Float(remoteConfig.Element ("all"), "size");
 			var nodes = remoteConfig.Element ("all").Elements();
 			foreach (XElement node in nodes) {
-				filenames.Add (node.Value);
+				filenames.Add (node.Value, Xml.Float(node, "size"));
 			}
 		} else{
 			string localVersion = Xml.Version (localConfig);
@@ -66,12 +66,12 @@ public class ConfigLoader
 			string remoteVersion = Xml.Version (remoteConfig);
 			//float filesize = Xml.Float(remoteConfig, "size");
 			//if (localVersion != remoteVersion) {
-				if(true){
-				fileSize = Xml.Float(remoteConfig.Element ("update"), "size");
+			if(true){
+				//fileSize = Xml.Float(remoteConfig.Element ("update"), "size");
 				var nodes = remoteConfig.Element ("update").Elements();
 				//List<string> updates = new List<string> ();
 				foreach (XElement node in nodes) {
-					filenames.Add (node.Value);
+					filenames.Add (node.Value, Xml.Float(node, "size"));
 				}
 				int idx = url.IndexOf ("/");
 				string path = idx == -1 ? url : url.Substring (0, idx);
@@ -80,23 +80,25 @@ public class ConfigLoader
 					yield return Request.ReadRemote (path+"/version/" + preVersion +".xml", str => tempConfig = XDocument.Parse(str).Root);
 					if (tempConfig == null) {
 						var all = remoteConfig.Element ("all").Elements ();
-						filenames = new List<string> ();
+						filenames = new Dictionary<string, float> ();
 						foreach (XElement node in all) {
-							filenames.Add (node.Value);
+							filenames.Add (node.Value, Xml.Float(node, "size"));
 						}
 						break;
 					} else {
 						preVersion = Xml.Attribute (tempConfig, "preversion");
 						var updateNotes = tempConfig.Element ("update").Elements();
 						foreach (XElement node in updateNotes) {
-							Logger.Log (node.Value,"blue");
-							if(!filenames.Contains(node.Value))
-								filenames.Add (node.Value);
+							//Logger.Log (node.Value,"blue");
+							if(!filenames.ContainsKey(node.Value))
+								filenames.Add (node.Value, Xml.Float(node, "size"));
 						}
 					}
 				}
 			}
 		}
+		foreach (string key in filenames.Keys)
+			fileSize += filenames [key];
 		if (okCancelPanel != null && Application.internetReachability != NetworkReachability.ReachableViaLocalAreaNetwork && fileSize > 3) {
 			//if (panel != null && filesize > 3) {
 			yield return okCancelPanel.Show (string.Format( I18n.Translate ("not_in_wifi"), fileSize.ToString()+"M"));
@@ -111,31 +113,29 @@ public class ConfigLoader
 		Request.Cancel();
 	}
 
-	private IEnumerator LoadFiles(List<string> names, string configurl){
-		if (names.Count == 0)
+	private IEnumerator LoadFiles(Dictionary<string, float> files, string configurl){
+		if (files.Count == 0)
 			yield break;
-		for (int i = 0; i < names.Count; i++) {
-			Logger.Log (names [i], "green");
-		}
 		//var nodes = remoteConfig.Elements ();
 		string platform = GetPlatformName ();
-		int count = names.Count;
+		int count = files.Count;
 		if (fileLoadedHandler != null && count>0) {
 			fileLoadedHandler.Invoke (0, count);
 		}
-		for (int i=0; i<count ; i++) {
-			string name = names [i];
+		int i = 1;
+		foreach (string name in files.Keys) {
 			yield return Request.DownloadFile (name.Replace ("{%platform%}", platform), name.Replace ("{%platform%}/", ""), false, progress=>{
 				if(progressHandler!=null)
-					progressHandler.Invoke(i+1, count, progress);
+					progressHandler.Invoke(i, count, progress);
 			});
 			if (forceBreak) {
 				yield break;
 			} else {
 				if (fileLoadedHandler != null) {
-					fileLoadedHandler.Invoke (i+1, count);
+					fileLoadedHandler.Invoke (i, count);
 				}
 			}
+			i++;
 		}
 		//Debug.Log ("remoteConfig: " + remoteConfig.ToString ());
 		File.WriteAllText (Path.Combine(Application.persistentDataPath,  configurl), remoteConfig.ToString());
