61e8d93a4494e4ba03e7a88a141bdebecf32b9cf
cx <chixuchixu@hotmail.com>
2017-09-14 16:25:57 +0800

    revert to the original scale and rotation after lost tracking

--- Scripts/Scenes/ScanSceneState/ScanObjectState.cs
+++ Scripts/Scenes/ScanSceneState/ScanObjectState.cs
@@ -12,6 +12,10 @@ using System.Xml.Linq;
 public class ScanObjectState:ScanSceneState
 {
 	private AudioSource audio;
+	private Vector3 localScale;
+	private Quaternion localRotation;
+	private Transform firstChild;
+	//private Vector3 localScale;
 
 	public ScanObjectState(){
 		name = "object";
@@ -23,12 +27,21 @@ public class ScanObjectState:ScanSceneState
 		TrackStart (ScanSceneController.currentTrackableObject.name, "Object");
 		if (audio)
 			audio.Play ();
+		firstChild = ScanSceneController.currentTrackableObject.transform.GetChild (0);
+		if (firstChild != null) {
+			localScale = firstChild.localScale;
+			firstChild.localRotation = Quaternion.identity;
+		}
 	}
 
 	public override void OnExit(){
 		TrackEnd (ScanSceneController.currentTrackableObject.name, "Object");
 		if (audio)
 			audio.Stop ();
+		if (firstChild != null && localScale!=null) {
+			firstChild.localScale = localScale;
+			//firstChild.localRotation = localRotation;
+		}
 		base.OnExit ();
 	}
 }
